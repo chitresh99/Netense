0ac8e314f23341e032cbcffb170aefddaf2b6d2e
chitresh99 <chitreshpoojary990@gmail.com>
2025-06-22 18:07:21 +0530

    feat(about) : added about.sh for project description

---
+++ about.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+greeting="Hello"
+name=$(whoami)
+date=$(date +%A)
+Feature1="1.Scan ip ranges for open ports"
+
+echo "$greeting $name.Today is $date."
+echo "Netense is a network tool which can help with your various networking activities"
+echo "Following are the list of features currently available"
+echo "-------------------------------------------------------"
+echo "$Feature1"
+
+
+
+
